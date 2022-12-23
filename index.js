03f8bcb0053c7741ddcf2024f35e115b2c3ea781
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-22 21:16:22 -0500

    commit tree 74a61a7e44d22b3a2dc5feb22f2c32aab0593cef

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+/// index.js
+
+const writeCards = (names) => {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  
+  const countDown = (num) => {
+    if (num < 0) {
+      return;
+    }
+  
+    console.log(num);
+    countDown(num - 1);
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
+  
